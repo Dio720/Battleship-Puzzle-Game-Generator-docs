d312d11676401eea908369e882fe643c16bb5a45
Vinicius de Lima <vinicius.lima.115@ufrn.edu.br>
2024-04-02 23:18:07 -0300

    Docs update

--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['validate_5finput_0',['validate_input',['../_validation_8h.html#ad4dcbc0c3a8ef09c76740f6d194d16b0',1,'validate_input(int argc, char *argv[]):&#160;Validation.cpp'],['../_validation_8cpp.html#ad4dcbc0c3a8ef09c76740f6d194d16b0',1,'validate_input(int argc, char *argv[]):&#160;Validation.cpp']]],
+  ['validate_5finput_0',['validate_input',['../_validation_8h.html#ab83cdc66574dfa58d3dce8008ec65288',1,'validate_input(int argc, char *argv[]):&#160;Validation.cpp'],['../_validation_8cpp.html#ab83cdc66574dfa58d3dce8008ec65288',1,'validate_input(int argc, char *argv[]):&#160;Validation.cpp']]],
   ['validate_5fnumber_5fof_5fpuzzles_1',['validate_number_of_puzzles',['../_validation_8h.html#a8796be10bc01244dae02d414e28d617e',1,'validate_number_of_puzzles(const std::string &amp;puzzles_count_str, RunningOpt &amp;run_opt):&#160;Validation.cpp'],['../_validation_8cpp.html#a8796be10bc01244dae02d414e28d617e',1,'validate_number_of_puzzles(const std::string &amp;puzzles_count_str, RunningOpt &amp;run_opt):&#160;Validation.cpp']]],
   ['validate_5frows_5fand_5fcols_5finput_2',['validate_rows_and_cols_input',['../_validation_8h.html#af18f7a4d725654be5ad89e464f33f294',1,'validate_rows_and_cols_input(const std::string &amp;dimension_type, const std::string &amp;dimension_value_str, RunningOpt &amp;run_opt):&#160;Validation.cpp'],['../_validation_8cpp.html#af18f7a4d725654be5ad89e464f33f294',1,'validate_rows_and_cols_input(const std::string &amp;dimension_type, const std::string &amp;dimension_value_str, RunningOpt &amp;run_opt):&#160;Validation.cpp']]]
 ];
